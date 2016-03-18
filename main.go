8f2458854239d3913278ce54ece5bd225c0ae80c
= <=>
2016-03-19 06:16:45 +0800

    Change the tab to blanks

--- main.go
+++ main.go
@@ -16,16 +16,16 @@ func main() {
   app.Usage = "A simple REST server!"
 
   app.Commands = []cli.Command{
-  	{
-   		Name:      "start",
-    	Aliases:     []string{"s"},
-    	Usage:     "start the web server",
-    	Action: func(c *cli.Context) {
-    		println("Visit the web: http://127.0.0.1:8080")
-    		http.HandleFunc("/",server.Index)
-    		log.Fatal(http.ListenAndServe(":8080", nil))
-    	},
-   	},
+    {
+      Name:      "start",
+      Aliases:     []string{"s"},
+      Usage:     "start the web server",
+      Action: func(c *cli.Context) {
+        println("Visit the web: http://127.0.0.1:8080")
+        http.HandleFunc("/",server.Index)
+        log.Fatal(http.ListenAndServe(":8080", nil))
+      },
+    },
   }
 
   app.Action = func(c *cli.Context) {
