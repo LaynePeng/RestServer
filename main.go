3f8e04fafd402f7403c023ae5342659f44283e3f
= <=>
2016-03-19 06:19:51 +0800

    Update Godeps to make self dependences.

--- main.go
+++ main.go
@@ -1,37 +1,37 @@
 package main
 
 import (
-  "os"
-  "log"
-  "net/http"
+    "os"
+    "log"
+    "net/http"
 
-  "github.com/codegangsta/cli"
+    "github.com/codegangsta/cli"
 
-  "github.com/LaynePeng/RestServer/server"
+    "github.com/LaynePeng/RestServer/server"
 )
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "My Server"
-  app.Usage = "A simple REST server!"
+    app := cli.NewApp()
+    app.Name = "My Server"
+    app.Usage = "A simple REST server!"
 
-  app.Commands = []cli.Command{
-    {
-      Name:      "start",
-      Aliases:     []string{"s"},
-      Usage:     "start the web server",
-      Action: func(c *cli.Context) {
-        println("Visit the web: http://127.0.0.1:8080")
-        http.HandleFunc("/",server.Index)
-        log.Fatal(http.ListenAndServe(":8080", nil))
-      },
-    },
-  }
+    app.Commands = []cli.Command{
+        {
+            Name:      "start",
+            Aliases:     []string{"s"},
+            Usage:     "start the web server",
+            Action: func(c *cli.Context) {
+                println("Visit the web: http://127.0.0.1:8080")
+                http.HandleFunc("/",server.Index)
+                log.Fatal(http.ListenAndServe(":8080", nil))
+            },
+        },
+    }
 
-  app.Action = func(c *cli.Context) {
-    println("Hello friend!")
-  }
+    app.Action = func(c *cli.Context) {
+        println("Hello friend!")
+    }
 
-  app.Run(os.Args)
+    app.Run(os.Args)
 }
 
