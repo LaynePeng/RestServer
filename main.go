fc478072c6e9650d34725f8053fff6fab6fc4a93
= <=>
2016-03-20 02:38:48 +0800

    Option to change the listening port and given a config file but not used yet.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "os"
     "log"
     "net/http"
+    "fmt"
 
     "github.com/codegangsta/cli"
 
@@ -11,9 +12,21 @@ import (
 )
 
 func main() {
+    var config string
+    var port int
+
     app := cli.NewApp()
     app.Name = "My Server"
     app.Usage = "A simple REST server!"
+
+    app.Flags = []cli.Flag {
+        cli.StringFlag{
+            Name: "config",
+            Value: "./server.conf",
+            Usage: "Config file for the server",
+            Destination: &config,
+        },
+    }
 
     app.Commands = []cli.Command{
         {
@@ -21,9 +34,19 @@ func main() {
             Aliases:     []string{"s"},
             Usage:     "start the web server",
             Action: func(c *cli.Context) {
-                println("Visit the web: http://127.0.0.1:8080")
+                portStr := fmt.Sprintf(":%d", port)
+                println(fmt.Sprintf("Visit the web: http://127.0.0.1%s", portStr))
                 http.HandleFunc("/",server.Index)
-                log.Fatal(http.ListenAndServe(":8080", nil))
+                log.Fatal(http.ListenAndServe(portStr, nil))
+            },
+
+            Flags: []cli.Flag{
+                cli.IntFlag{
+                    Name: "port",
+                    Value: 8080,
+                    Usage: "Listening port",
+                    Destination: &port,
+                },
             },
         },
     }
